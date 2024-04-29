cc9ea34ce309d9ef4e291e47f4f507494ef95f61
Sky Stebnicki <sky@stebnicki.net>
2024-04-29 06:03:35 -0400

    Removed overlapping shortcut

--- lua/sky/remap.lua
+++ lua/sky/remap.lua
@@ -1,4 +1,3 @@
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>");
 vim.keymap.set("n", "<C-T>", "<cmd>silent !tmux splitw -h -p 30<CR>");
 vim.keymap.set('n', '<C-E>', '<Cmd>NvimTreeToggle<CR>', { desc = '[T]ree' })
-vim.keymap.set('n', '<leader>e', '<Cmd>NvimTreeToggle<CR>', { desc = '[T]ree' })
